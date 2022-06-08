bcb4fc340a7992850a92af268db5aa8acde8e219
Jane Bob Doe <szandra.koma@gmail.com>
2022-06-08 10:13:57 +0200

    flyway

--- src/main/resources/db/migration/V2__create_presentation_table.sql
+++ src/main/resources/db/migration/V2__create_presentation_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE presentation (
     id integer NOT NULL AUTO_INCREMENT,
     lecturer_id integer,
     title varchar(255),
-    start_time date,
+    start_time datetime,
     PRIMARY KEY (id)
 );
 
