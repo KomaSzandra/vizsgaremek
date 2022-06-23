75edf96f1f1a05a3e1a9cd7e3e53e4277a2e4f6b
Jane Bob Doe <szandra.koma@gmail.com>
2022-06-23 20:03:09 +0200

    fixes

--- src/main/java/hu/progmasters/conference/controller/LecturerController.java
+++ src/main/java/hu/progmasters/conference/controller/LecturerController.java
@@ -40,7 +40,7 @@ public class LecturerController {
     @ApiResponse(responseCode = "201", description = "Lecturer has been saved")
     @ApiResponse(responseCode = "400", description = "Bad request, lecturer cannot be created")
     public ResponseEntity<LecturerInfo> saveLecturer(@Valid @RequestBody LecturerCreateCommand command) {
-        LOGGER.info(LOG_POST, String.format(command.toString()));
+        LOGGER.info(LOG_POST, command.toString());
         LecturerInfo saved = lecturerService.saveLecturer(command);
         LOGGER.info(String.format(HTTP_RESPONSE, "CREATED", saved));
         return new ResponseEntity<>(saved, HttpStatus.CREATED);
@@ -86,11 +86,11 @@ public class LecturerController {
     @Operation(summary = "Set the lecturer for the presentation")
     @ApiResponse(responseCode = "200", description = "Lecturer has been updated")
     @ApiResponse(responseCode = "400", description = "Bad request, lecturer cannot be updated")
-    public LecturerInfo update(@PathVariable("id") Integer id, @Valid @RequestBody LecturerUpdateCommand command) {
+    public ResponseEntity<LecturerInfo> update(@PathVariable("id") Integer id, @Valid @RequestBody LecturerUpdateCommand command) {
         LOGGER.info(String.format(LOG_PUT, "/" + id, command.toString()));
         LecturerInfo lecturerInfo = lecturerService.addLecturerToPresentation(id, command);
         LOGGER.info(String.format(HTTP_RESPONSE, "OK", ""));
-        return lecturerInfo;
+        return new ResponseEntity<>(lecturerInfo, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
--- src/main/java/hu/progmasters/conference/controller/ParticipationController.java
+++ src/main/java/hu/progmasters/conference/controller/ParticipationController.java
@@ -39,7 +39,7 @@ public class ParticipationController {
     @ApiResponse(responseCode = "201", description = "Participation has been saved")
     @ApiResponse(responseCode = "400", description = "Bad request, participation cannot be saved")
     public ResponseEntity<ParticipationInfo> registrate(@Valid @RequestBody ParticipationCreateCommand command) {
-        LOGGER.info(LOG_POST, String.format(command.toString()));
+        LOGGER.info(LOG_POST, command.toString());
         ParticipationInfo saved = participationService.registrate(command);
         LOGGER.info(String.format(HTTP_RESPONSE, "CREATED", saved));
         return new ResponseEntity<>(saved, HttpStatus.CREATED);
@@ -47,43 +47,39 @@ public class ParticipationController {
 
     @GetMapping
     @Operation(summary = "Lists all participations")
-    @ResponseStatus(HttpStatus.OK)
     @ApiResponse(responseCode = "200", description = "Participations have been listed")
     @ApiResponse(responseCode = "400", description = "Bad request, participations cannot be listed")
-    public List<ParticipationInfo> findAll() {
+    public ResponseEntity<List<ParticipationInfo>> findAll() {
         LOGGER.info(String.format(LOG_GET, ""));
         List<ParticipationInfo> infos = participationService.findAll();
         LOGGER.info(String.format(HTTP_RESPONSE, "OK", infos));
-        return infos;
+        return new ResponseEntity<>(infos, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     @Operation(summary = "Get a participation by given id")
-    @ResponseStatus(HttpStatus.OK)
     @ApiResponse(responseCode = "200", description = "Participation has been found")
     @ApiResponse(responseCode = "400", description = "Bad request, participation cannot be found")
-    public ParticipationInfo findById(@PathVariable("id") Integer id) {
+    public ResponseEntity<ParticipationInfo> findById(@PathVariable("id") Integer id) {
         LOGGER.info(String.format(LOG_GET, "/" + id));
         ParticipationInfo byId = participationService.findById(id);
         LOGGER.info(String.format(HTTP_RESPONSE, "OK", byId));
-        return byId;
+        return new ResponseEntity<>(byId, HttpStatus.OK);
     }
 
     @GetMapping("findAllByParticipant")
     @Operation(summary = "Get participations by participant")
-    @ResponseStatus(HttpStatus.OK)
     @ApiResponse(responseCode = "200", description = "Participations have been listed")
     @ApiResponse(responseCode = "400", description = "Bad request, participations cannot be listed")
-    public List<ParticipationInfo> findAllByParticipant(@RequestParam("participantId") Integer participantId) {
+    public ResponseEntity<List<ParticipationInfo>> findAllByParticipant(@RequestParam("participantId") Integer participantId) {
         LOGGER.info(String.format(LOG_GET, "/" + participantId));
         List<ParticipationInfo> participations = participationService.findByParticipant(participantId);
         LOGGER.info(String.format(HTTP_RESPONSE, "OK", participations));
-        return participations;
+        return new ResponseEntity<>(participations, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     @Operation(summary = "Delete a participation")
-    @ResponseStatus(HttpStatus.OK)
     @ApiResponse(responseCode = "200", description = "Participations has been deleted")
     @ApiResponse(responseCode = "400", description = "Bad request, participations cannot be deleted")
     public ResponseEntity<Void> delete(@PathVariable("id") Integer id) {
@@ -94,13 +90,13 @@ public class ParticipationController {
     }
 
     @PutMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "Transferring a participant's participation to another presentation")
     @ApiResponse(responseCode = "200", description = "Participation has been updated")
     @ApiResponse(responseCode = "400", description = "Bad request, participation cannot be updated")
-    public ParticipationInfo updateParticipantsPresentation(@PathVariable("id") Integer id,
-                                                          @RequestBody ParticipationUpdateCommand command) {
+    public ResponseEntity<ParticipationInfo> updateParticipantsPresentation(@PathVariable("id") Integer id, @Valid @RequestBody ParticipationUpdateCommand command) {
         LOGGER.info(String.format(LOG_PUT, "/" + id, command.toString()));
-        return participationService.updateParticipantsPresentation(id, command);
+        ParticipationInfo info = participationService.updateParticipantsPresentation(id, command);
+        LOGGER.info(String.format(HTTP_RESPONSE, "OK", ""));
+        return new ResponseEntity<>(info, HttpStatus.OK);
     }
 }
